b574dc7a1b4dc41be4b1798bfc5910014afb78de
rashik007 <rashikarmacharya@gmail.com>
2024-08-13 16:52:02 +0545

    added multiple pages | changed menu | completed homepage and rooms and suites page

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/******************************
+  SWIPER HOMEPAGE 
+* ***************************/
 const swiper = new Swiper(".rooms__slider", {
   // Optional parameters
   direction: "horizontal",
@@ -22,11 +25,69 @@ const swiper = new Swiper(".rooms__slider", {
   },
 });
 
+
+/**************************************
+ *  SWIPER ROOMS AND SUITES PAGE 
+ * 
+ * ***********************************/
+const swiper2 = new Swiper(".room_types", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 40,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+
+// rooms and suites slider for bed and bath
+const swiper3 = new Swiper(".feature__slider", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 40,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+
 /**********************************
  *  FLATPICKER
- *********************************/
+//  *********************************/
 
-flatpickr("input[type=datetime-local]", {});
+// flatpickr("input[type=datetime-local]", {});
+
+// $(function () {
+//   $(".date-picker").datepicker({
+//     dateFormat: "D, d M",
+//   });
+// });
 
 /**************************************
  *  HEADER NAVIGATION POSITION FIXED
@@ -40,3 +101,45 @@ window.addEventListener("scroll", function () {
     menu.classList.remove("scrolled");
   }
 });
+
+
+/************** search tbutton ************** */
+const btnSearch = document.querySelector('.Btn__search__top');
+const inputForButton = document.querySelector('input.search__btn__top');
+const searchForm = document.querySelector('.search__top__')
+let searchActive = false;
+
+btnSearch.addEventListener('click', (e)=> {
+  e.preventDefault();
+  if(inputForButton.classList.contains("hide")){
+    console.log('button is hide');
+    inputForButton.classList.add('show');
+    inputForButton.classList.remove('hide');
+    
+    inputForButton.click();
+    searchActive = true;
+
+  }
+})
+
+// btnSearch.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   console.log(searchActive);
+//   if(searchActive && inputForButton.classList.contains("show")) {
+//     console.log('search is active and ');
+//     searchActive = false;
+//     inputForButton.classList.remove('show');
+//     inputForButton.classList.add('hide');
+//   }
+// })
+
+
+
+document.addEventListener('click', (e) => {
+  if(searchActive && !btnSearch.contains(e.target) && !searchForm.contains(e.target)) {
+    inputForButton.classList.remove('show');
+    inputForButton.classList.add('hide');
+    searchActive = false;
+    inputForButton.value= '';
+  }
+})
