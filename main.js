48e8f93db8f9bfb70eacf3c53385877468f1b947
rashik007 <rashikarmacharya@gmail.com>
2024-08-20 17:57:36 +0545

    added about.html | contact.html | reposnsive menu applied in index.html | responsive website started in index.html

--- main.js
+++ main.js
@@ -5,8 +5,23 @@ const swiper = new Swiper(".rooms__slider", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
-  slidesPerView: 3,
-  spaceBetween: 40,
+
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 20,    
+    },
+
+    800: {
+      slidesPerView: 3,
+      spaceBetween: 40,    
+    },
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 40,    
+    }
+  },
+ 
 
   // If we need pagination
   pagination: {
@@ -77,6 +92,27 @@ const swiper3 = new Swiper(".feature__slider", {
 });
 
 
+// rooms and suites slider for bed and bath
+const swiper4 = new Swiper(".room_single_slider", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
 /**********************************
  *  FLATPICKER
 //  *********************************/
@@ -143,3 +179,132 @@ document.addEventListener('click', (e) => {
     inputForButton.value= '';
   }
 })
+
+
+/******  datepicker  ******/
+$(document).ready(function () {
+ 
+  $(function () {
+      $(".date-picker").
+      datepicker();
+  });
+}) 
+
+
+/********* get today's date ********* */
+const today = new Date();
+
+const month = String(today.getMonth() + 1 ).padStart(2,'0');
+const day = String(today.getDay()).padStart(2, '0');
+const year = today.getFullYear();
+
+const formattedDate = `${month}/${day}/${year}`;
+
+console.log(formattedDate);
+
+document.querySelector('.date__input__').placeholder = formattedDate;
+
+
+console.log(today);
+
+
+/********************************
+ * 
+ * MOBILE NAVIGATION
+ * 
+ ********************************/
+//get hamburger button'
+
+  const hamButton = document.querySelector('.hamburger');
+  
+  //get nav links class
+  const navigationLinks = document.querySelector('.nav__link__prime');
+  
+  hamButton.addEventListener('click', () => {
+    // e.preventDefault();
+  
+    if(navigationLinks.classList.contains('mobile__nav__links')) {
+      navigationLinks.classList.add('mobile__nav__links');
+    }
+  
+    else  {
+      navigationLinks.classList.remove('mobile__nav__links');
+      console.log('navigation button was clicked');
+    }
+  })
+
+
+
+
+
+function setLayout() {
+  // const screenSize = window.innerWidth; 
+  const navigationLinks = document.querySelector('.nav__link__prime');
+  let clicker = false; 
+  // if(navigationLinks) {
+
+  function handleInteraction(event) {
+    if(clicker && !navigationLinks.contains(event.target)) {
+      // console.log('click or tought detected');
+      navigationLinks.classList.remove('active');
+      // console.log('active class removed');
+      clicker = false;
+    }
+  }
+
+  if (navigationLinks.classList.contains('active')) {
+    navigationLinks.classList.remove('active');   
+    clicker = false;
+
+    window.removeEventListener('click', handleInteraction);
+    window.removeEventListener('touchstart', handleInteraction);
+  }
+  else {
+    navigationLinks.classList.add('active');
+    clicker = true;
+    // console.log('active class is added');
+
+    setTimeout(() => {
+      window.addEventListener('click', handleInteraction);
+      window.addEventListener('touchstart', handleInteraction);
+    }, 0);
+
+    }
+} 
+
+
+
+/************* ADD OR REMOVE CLASS DEPENDING ON DEVICE WIDTH *********** */
+function toggleClassOnWidth() {
+  const element = document.querySelector('.nav_links.nav__link__prime');
+  const mobileClass = 'mobile__nav__links';
+
+  const mobileWidth = 768;
+
+  if(window.innerWidth <= mobileWidth) {
+    //if device width is smallers tha 768px;
+    element.classList.add(mobileClass);
+  }
+
+  else {
+    //if device width is greater thatn 768 
+    element.classList.remove(mobileClass);
+  }
+
+}
+
+//run this function on page load
+toggleClassOnWidth();
+
+//run on windows resize
+window.addEventListener('resize', toggleClassOnWidth());
+
+
+/**************** button default behavior 
+ *  ************* */
+let buttonsAll = document.querySelectorAll('button');
+buttonsAll.forEach(button => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+  })
+});
